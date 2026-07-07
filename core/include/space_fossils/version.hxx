321149708598eb6dbc516247e85f3283dc9c5577
lexar30 <vladislavbernikov@gmail.com>
2026-07-07 19:46:57 +0300

    Added version specs

---
+++ core/include/space_fossils/version.hxx
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstdint>
+
+namespace space_fossils::core::version {
+	inline constexpr const char* ApplicationName = "space-fossils";
+	inline constexpr const char* ApplicationVersion = "1.0.0-pre-alpha";
+	inline constexpr std::uint32_t SnapshotFormatVersion = 1;
+}
