2ba313aee93aacdf1cabc3021a0ff7787b0ebff2
lexar30 <vladislavbernikov@gmail.com>
2026-07-15 15:14:57 +0300

    Added appstate

--- cli/include/space_fossils/cli/app_state.hxx
+++ cli/include/space_fossils/cli/app_state.hxx
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "space_fossils/core/file_tree/storage/storage.hxx"
+#include "space_fossils/core/file_tree/session/session.hxx"
+
 namespace space_fossils::cli {
+	using space_fossils::core::file_tree::Storage;
+	using space_fossils::core::file_tree::Session;
+
 	struct AppState
 	{
+		Storage storage;
+		Session session;
+
+		bool isQuitRequested = false;
+		bool isStorageFilled = false;
 	};
 }
