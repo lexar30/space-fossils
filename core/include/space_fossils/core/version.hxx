8cfbc12c09b7ac8167dfdb7039890e50daa3e52d
lexar30 <vladislavbernikov@gmail.com>
2026-07-19 00:10:56 +0300

    Promoted to alpha

--- core/include/space_fossils/core/version.hxx
+++ core/include/space_fossils/core/version.hxx
@@ -4,6 +4,6 @@
 
 namespace space_fossils::core::version {
 	inline constexpr const char* ApplicationName = "space-fossils";
-	inline constexpr const char* ApplicationVersion = "1.0.0-pre-alpha";
+	inline constexpr const char* ApplicationVersion = "1.0.0-alpha";
 	inline constexpr std::uint32_t SnapshotFormatVersion = 1;
 }
